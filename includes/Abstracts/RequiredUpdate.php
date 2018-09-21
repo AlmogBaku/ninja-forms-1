8a4ef5cfa980a150207893168920d9590df7943b
KR Moorhouse <spkr4thded@gmail.com>
2018-09-21 13:40:53 +0000

    Migrated shared functions and variables of the required updates to parent class.

--- includes/Abstracts/RequiredUpdate.php
+++ includes/Abstracts/RequiredUpdate.php
@@ -12,18 +12,33 @@ abstract class NF_Abstracts_RequiredUpdate
 
     protected $_class_name = '';
 
-    public $debug = false;
+    protected $db;
 
     public $response = array();
 
+    public $debug = false;
+    
+    public $lock_process = false;
 
     /**
      * Constructor
      */
     public function __construct( $data = array() )
     {
+        // Save a reference to wpdb.
+        global $wpdb;
+        $this->db = $wpdb;
         //Bail if we aren't in the admin.
         if ( ! is_admin() ) return false;
+        // If we weren't provided with a slug or a class name...
+        if ( ! isset( $data[ 'slug' ] ) || ! isset( $data[ 'class_name' ] ) ) {
+            // Bail.
+            return false;
+        }
+        $this->_slug = $data[ 'slug' ];
+        $this->_class_name = $data[ 'class_name' ];
+        // Record debug settings if provided.
+        if ( isset( $data[ 'debug' ] ) ) $this->debug = $data[ 'debug' ];
     }
 
 
@@ -75,4 +90,59 @@ abstract class NF_Abstracts_RequiredUpdate
         die();
     }
 
+
+    /**
+     * Function to run our table migrations.
+     * 
+     * @param $callback (String) The callback function in the migration file.
+     * 
+     * @since UPDATE_VERSION_ON_MERGE
+     */
+    protected function migrate( $callback )
+    {
+        $migrations = new NF_Database_Migrations();
+        $migrations->do_upgrade( $callback );
+    }
+
+    /**
+     * Function to prepare our query values for insert.
+     * 
+     * @param $value (Mixed) The value to be escaped for SQL.
+     * @return (String) The escaped (and possibly serialized) value of the string.
+     * 
+     * @since UPDATE_VERSION_ON_MERGE
+     */
+    public function prepare( $value )
+    {
+        // If our value is a number...
+        if ( is_float( $value ) ) {
+            // Exit early and return the value.
+            return $value;
+        }
+        // Serialize the value if necessary.
+        $escaped = maybe_serialize( $value );
+        // Escape it.
+        $escaped = $this->db->_real_escape( $escaped );
+
+        return $escaped;
+    }
+
+    /**
+     * Function used to call queries that are gated by debug.
+     * 
+     * @param $sql (String) The query to be run.
+     * @return (Object) The response to the wpdb query call.
+     * 
+     * @since UPDATE_VERSION_ON_MERGE
+     */
+    protected function query( $sql )
+    {
+        // If we're not debugging...
+        if ( ! $this->debug ) {
+            // Run the query.
+            return $this->db->query( $sql );
+        }
+        // Otherwise, return false.
+        return false;
+    }
 }
