65551d7b5a74756578e70a6475e19f1834c2ae75
Kevin Stover <kstover@gmail.com>
2018-10-18 10:39:27 -0400

    Adding support for 2.9.x form imports.

--- includes/Admin/Processes/ImportForm.php
+++ includes/Admin/Processes/ImportForm.php
@@ -74,8 +74,23 @@ class NF_Admin_Processes_ImportForm extends NF_Abstracts_BatchProcess
 
         $data = explode( ';base64,', $_POST[ 'extraData' ][ 'content' ] );
         $data = base64_decode( $data[ 1 ] );
+        
+        /**
+         * $data could now hold two things, depending on whether this was a 2.9 or 3.0 export.
+         * 
+         * If it's a 3.0 export, the data will be json encoded.
+         * If it's a 2.9 export, the data will be serialized.
+         *
+         * We're first going to try to json_decode. If we don't get an array, we'll unserialize.
+         */
+
         $decoded_data = json_decode( WPN_Helper::json_cleanup( html_entity_decode( $data ) ), true );
         
+        // If we don't have an array, try unserializing
+        if ( ! is_array( $decoded_data ) ) {
+            $decoded_data = WPN_Helper::maybe_unserialize( $data );
+        }
+
         // Try to utf8 decode our results.
         $data = WPN_Helper::utf8_decode( $decoded_data );
 
@@ -83,6 +98,8 @@ class NF_Admin_Processes_ImportForm extends NF_Abstracts_BatchProcess
         if ( ! json_encode( $data ) ) {
             $data = $decoded_data;
         }
+
+        $data = $this->import_form_backwards_compatibility( $data );
 
         // $data is now a form array.
         $this->form = $data;
@@ -368,6 +385,8 @@ class NF_Admin_Processes_ImportForm extends NF_Abstracts_BatchProcess
             if ( ! isset ( $this->form[ 'fields' ][ $i ] ) ) continue;
 
             $field_settings = $this->form[ 'fields' ][ $i ];
+            // Remove a field ID if we have one set.
+            unset( $field_settings[ 'id' ] );
             $field_settings[ 'parent_id' ] = $this->form[ 'ID' ];
             // Array that tracks which settings need to be meta and which are columns.
             $field_meta = $field_settings;
@@ -423,5 +442,415 @@ class NF_Admin_Processes_ImportForm extends NF_Abstracts_BatchProcess
             // Remove this field from our fields array.
             unset( $this->form[ 'fields' ][ $i ] );
         }
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backwards Compatibility
+    |--------------------------------------------------------------------------
+    */
+
+    public function import_form_backwards_compatibility( $import )
+    {
+        // Rename `data` to `settings`
+        if( isset( $import[ 'data' ] ) ){
+            $import[ 'settings' ] = $import[ 'data' ];
+            unset( $import[ 'data' ] );
+        }
+
+        // Rename `notifications` to `actions`
+        if( isset( $import[ 'notifications' ] ) ){
+            $import[ 'actions' ] = $import[ 'notifications' ];
+            unset( $import[ 'notifications' ] );
+        }
+
+        // Rename `form_title` to `title`
+        if( isset( $import[ 'settings' ][ 'form_title' ] ) ){
+            $import[ 'settings' ][ 'title' ] = $import[ 'settings' ][ 'form_title' ];
+            unset( $import[ 'settings' ][ 'form_title' ] );
+        }
+
+        // Convert `last_sub` to `_seq_num`
+        if( isset( $import[ 'settings' ][ 'last_sub' ] ) ) {
+            $import[ 'settings' ][ '_seq_num' ] = $import[ 'settings' ][ 'last_sub' ] + 1;
+        }
+
+        // Make sure
+        if( ! isset( $import[ 'fields' ] ) ){
+            $import[ 'fields' ] = array();
+        }
+
+        // `Field` to `Fields`
+        if( isset( $import[ 'field' ] ) ){
+            $import[ 'fields' ] = $import[ 'field' ];
+            unset( $import[ 'field' ] );
+        }
+
+        $import = apply_filters( 'ninja_forms_upgrade_settings', $import );
+
+        // Combine Field and Field Data
+        foreach( $import[ 'fields' ] as $key => $field ){
+
+            if( '_honeypot' == $field[ 'type' ] ) {
+                unset( $import[ 'fields' ][ $key ] );
+                continue;
+            }
+
+            if( ! $field[ 'type' ] ) {
+                unset( $import[ 'fields'][ $key ] );
+                continue;
+            }
+
+            // TODO: Split Credit Card field into multiple fields.
+            $field = $this->import_field_backwards_compatibility( $field );
+
+            if( isset( $field[ 'new_fields' ] ) ){
+                foreach( $field[ 'new_fields' ] as $new_field ){
+                    $import[ 'fields' ][] = $new_field;
+                }
+                unset( $field[ 'new_fields' ] );
+            }
+
+            $import[ 'fields' ][ $key ] = $field;
+        }
+
+        $has_save_action = FALSE;
+        foreach( $import[ 'actions' ] as $key => $action ){
+            $action = $this->import_action_backwards_compatibility( $action );
+            $import[ 'actions' ][ $key ] = $action;
+
+            if( 'save' == $action[ 'type' ] ) $has_save_action = TRUE;
+        }
+
+        if( ! $has_save_action ) {
+            $import[ 'actions' ][] = array(
+                'type' => 'save',
+                'label' => __( 'Save Form', 'ninja-forms' ),
+                'active' => TRUE
+            );
+        }
+
+        $import = $this->import_merge_tags_backwards_compatibility( $import );
+
+        return apply_filters( 'ninja_forms_after_upgrade_settings', $import );
+    }
+
+    public function import_merge_tags_backwards_compatibility( $import )
+    {
+        $field_lookup = array();
+
+        foreach( $import[ 'fields' ] as $key => $field ){
+
+            if( ! isset( $field[ 'id' ] ) ) continue;
+
+            $field_id  = $field[ 'id' ];
+            $field_key = $field[ 'type' ] . '_' . $field_id;
+            $field_lookup[ $field_id ] = $import[ 'fields' ][ $key ][ 'key' ] = $field_key;
+        }
+
+        foreach( $import[ 'actions' ] as $key => $action_settings ){
+            foreach( $action_settings as $setting => $value ){
+                foreach( $field_lookup as $field_id => $field_key ){
+
+                    // Convert Tokenizer
+                    $token = 'field_' . $field_id;
+                    if( ! is_array( $value ) ) {
+                        if (FALSE !== strpos($value, $token)) {
+                            $value = str_replace($token, '{field:' . $field_key . '}', $value);
+                        }
+                    }
+
+                    // Convert Shortcodes
+                    $shortcode = "[ninja_forms_field id=$field_id]";
+                    if( ! is_array( $value ) ) {
+                        if ( FALSE !== strpos( $value, $shortcode ) ) {
+                            $value = str_replace( $shortcode, '{field:' . $field_key . '}', $value );
+                        }
+                    }
+                }
+
+                //Checks for the nf_sub_seq_num short code and replaces it with the submission sequence merge tag
+                $sub_seq = '[nf_sub_seq_num]';
+                if( ! is_array( $value ) ) {
+                    if( FALSE !== strpos( $value, $sub_seq ) ){
+                        $value = str_replace( $sub_seq, '{submission:sequence}', $value );
+                    }
+                }
+
+                if( ! is_array( $value ) ) {
+                    if (FALSE !== strpos($value, '[ninja_forms_all_fields]')) {
+                        $value = str_replace('[ninja_forms_all_fields]', '{field:all_fields}', $value);
+                    }
+                }
+                $action_settings[ $setting ] = $value;
+                $import[ 'actions' ][ $key ] = $action_settings;
+            }
+        }
+
+        return $import;
+    }
+
+    public function import_action_backwards_compatibility( $action )
+    {
+        // Remove `_` from type
+        if( isset( $action[ 'type' ] ) ) {
+            $action['type'] = str_replace('_', '', $action['type']);
+        }
+
+        if( 'email' == $action[ 'type' ] ){
+            $action[ 'to' ]             = str_replace( '`', ',', $action[ 'to' ] );
+            $action[ 'email_subject' ]  = str_replace( '`', ',', $action[ 'email_subject' ] );
+            $action[ 'cc' ]         = str_replace( '`', ',', $action[ 'cc' ] );
+            $action[ 'bcc' ]        = str_replace( '`', ',', $action[ 'bcc' ] );
+            // If our email is in plain text...
+            if ( $action[ 'email_format' ] == 'plain' ) {
+                // Record it as such.
+                $action[ 'email_message_plain' ] = $action[ 'email_message' ];
+            } // Otherwise... (It's not plain text.)
+            else {
+                // Record it as HTML.
+                $action[ 'email_message' ] = nl2br( $action[ 'email_message' ] );
+            }
+        }
+
+        // Convert `name` to `label`
+        if( isset( $action[ 'name' ] ) ) {
+            $action['label'] = $action['name'];
+            unset($action['name']);
+        }
+
+        return apply_filters( 'ninja_forms_upgrade_action_' . $action[ 'type' ], $action );
+    }
+
+    public function import_field_backwards_compatibility( $field )
+    {
+        // Flatten field settings array
+        if( isset( $field[ 'data' ] ) && is_array( $field[ 'data' ] ) ){
+            $field = array_merge( $field, $field[ 'data' ] );
+        }
+        unset( $field[ 'data' ] );
+
+        // Drop form_id in favor of parent_id, which is set by the form.
+        if( isset( $field[ 'form_id' ] ) ){
+            unset( $field[ 'form_id' ] );
+        }
+
+        // Remove `_` prefix from type setting
+        $field[ 'type' ] = ltrim( $field[ 'type' ], '_' );
+
+        // Type: `text` -> `textbox`
+        if( 'text' == $field[ 'type' ] ){
+            $field[ 'type' ] = 'textbox';
+        }
+
+        if( 'submit' == $field[ 'type' ] ){
+            $field[ 'processing_label' ] = 'Processing';
+        }
+
+        if( isset( $field[ 'email' ] ) ){
+
+            if( 'textbox' == $field[ 'type' ] && $field[ 'email' ] ) {
+                $field['type'] = 'email';
+            }
+            unset( $field[ 'email' ] );
+        }
+
+        if( isset( $field[ 'class' ] ) ){
+            $field[ 'element_class' ] = $field[ 'class' ];
+            unset( $field[ 'class' ] );
+        }
+
+        if( isset( $field[ 'req' ] ) ){
+            $field[ 'required' ] = $field[ 'req' ];
+            unset( $field[ 'req' ] );
+        }
+
+        if( isset( $field[ 'default_value_type' ] ) ){
+
+            /* User Data */
+            if( '_user_id' == $field[ 'default_value_type' ] )           $field[ 'default' ] = '{wp:user_id}';
+            if( '_user_email' == $field[ 'default_value_type' ] )        $field[ 'default' ] = '{wp:user_email}';
+            if( '_user_lastname' == $field[ 'default_value_type' ] )     $field[ 'default' ] = '{wp:user_last_name}';
+            if( '_user_firstname' == $field[ 'default_value_type' ] )    $field[ 'default' ] = '{wp:user_first_name}';
+            if( '_user_display_name' == $field[ 'default_value_type' ] ) $field[ 'default' ] = '{wp:user_display_name}';
+
+            /* Post Data */
+            if( 'post_id' == $field[ 'default_value_type' ] )    $field[ 'default' ] = '{wp:post_id}';
+            if( 'post_url' == $field[ 'default_value_type' ] )   $field[ 'default' ] = '{wp:post_url}';
+            if( 'post_title' == $field[ 'default_value_type' ] ) $field[ 'default' ] = '{wp:post_title}';
+
+            /* System Data */
+            if( 'today' == $field[ 'default_value_type' ] ) $field[ 'default' ] = '{other:date}';
+
+            /* Miscellaneous */
+            if( '_custom' == $field[ 'default_value_type' ] && isset( $field[ 'default_value' ] ) ){
+                $field[ 'default' ] = $field[ 'default_value' ];
+            }
+            if( 'querystring' == $field[ 'default_value_type' ] && isset( $field[ 'default_value' ] ) ){
+                $field[ 'default' ] = '{querystring:' . $field[ 'default_value' ] . '}';
+            }
+
+            unset( $field[ 'default_value' ] );
+            unset( $field[ 'default_value_type' ] );
+        } else if ( isset ( $field[ 'default_value' ] ) ) {
+            $field[ 'default' ] = $field[ 'default_value' ];
+        }
+
+        if( 'list' == $field[ 'type' ] ) {
+
+            if ( isset( $field[ 'list_type' ] ) ) {
+
+                if ('dropdown' == $field['list_type']) {
+                    $field['type'] = 'listselect';
+                }
+                if ('radio' == $field['list_type']) {
+                    $field['type'] = 'listradio';
+                }
+                if ('checkbox' == $field['list_type']) {
+                    $field['type'] = 'listcheckbox';
+                }
+                if ('multi' == $field['list_type']) {
+                    $field['type'] = 'listmultiselect';
+                }
+            }
+
+            if( isset( $field[ 'list' ][ 'options' ] ) ) {
+                $field[ 'options' ] = array_values( $field[ 'list' ][ 'options' ] );
+                unset( $field[ 'list' ][ 'options' ] );
+            }
+
+            foreach( $field[ 'options' ] as &$option ){
+                if( isset( $option[ 'value' ] ) && $option[ 'value' ] ) continue;
+                $option[ 'value' ] = $option[ 'label' ];
+            }
+        }
+
+        if( 'country' == $field[ 'type' ] ){
+            $field[ 'type' ] = 'listcountry';
+            $field[ 'options' ] = array();
+        }
+
+        // Convert `textbox` to other field types
+        foreach( array( 'fist_name', 'last_name', 'user_zip', 'user_city', 'user_phone', 'user_email', 'user_address_1', 'user_address_2', 'datepicker' ) as $item ) {
+            if ( isset( $field[ $item ] ) && $field[ $item ] ) {
+                $field[ 'type' ] = str_replace( array( '_', 'user', '1', '2', 'picker' ), '', $item );
+
+                unset( $field[ $item ] );
+            }
+        }
+
+        if( 'timed_submit' == $field[ 'type' ] ) {
+            $field[ 'type' ] = 'submit';
+        }
+
+        if( 'checkbox' == $field[ 'type' ] ){
+
+            if( isset( $field[ 'calc_value' ] ) ){
+
+                if( isset( $field[ 'calc_value' ][ 'checked' ] ) ){
+                    $field[ 'checked_calc_value' ] = $field[ 'calc_value' ][ 'checked' ];
+                    unset( $field[ 'calc_value' ][ 'checked' ] );
+                }
+                if( isset( $field[ 'calc_value' ][ 'unchecked' ] ) ){
+                    $field[ 'unchecked_calc_value' ] = $field[ 'calc_value' ][ 'unchecked' ];
+                    unset( $field[ 'calc_value' ][ 'unchecked' ] );
+                }
+            }
+        }
+
+        if( 'rating' == $field[ 'type' ] ){
+            $field[ 'type' ] = 'starrating';
+
+            if( isset( $field[ 'rating_stars' ] ) ){
+                $field[ 'default' ] = $field[ 'rating_stars' ];
+                unset( $field[ 'rating_stars' ] );
+            }
+        }
+
+        if( 'number' == $field[ 'type' ] ){
+
+            if( ! isset( $field[ 'number_min' ] ) || ! $field[ 'number_min' ] ){
+                $field[ 'num_min' ] = '';
+            } else {
+                $field[ 'num_min' ] = $field[ 'number_min' ];
+            }
+
+            if( ! isset( $field[ 'number_max' ] ) || ! $field[ 'number_max' ] ){
+                $field[ 'num_max' ] = '';
+            } else {
+                $field[ 'num_max' ] = $field[ 'number_max' ];
+            }
+
+            if( ! isset( $field[ 'number_step' ] ) || ! $field[ 'number_step' ] ){
+                $field[ 'num_step' ] = 1;
+            } else {
+                $field[ 'num_step' ] = $field[ 'number_step' ];
+            }
+        }
+
+        if( 'profile_pass' == $field[ 'type' ] ){
+            $field[ 'type' ] = 'password';
+
+            $passwordconfirm = array_merge( $field, array(
+                'id' => '',
+                'type' => 'passwordconfirm',
+                'label' => $field[ 'label' ] . ' ' . __( 'Confirm', 'ninja-forms' ),
+                'confirm_field' => 'password_' . $field[ 'id' ]
+            ));
+            $field[ 'new_fields' ][] = $passwordconfirm;
+        }
+
+        if( 'desc' == $field[ 'type' ] ){
+            $field[ 'type' ] = 'html';
+        }
+
+        if( 'credit_card' == $field[ 'type' ] ){
+
+            $field[ 'type' ] = 'creditcardnumber';
+            $field[ 'label' ] = $field[ 'cc_number_label' ];
+            $field[ 'label_pos' ] = 'above';
+
+            if( $field[ 'help_text' ] ){
+                $field[ 'help_text' ] = '<p>' . $field[ 'help_text' ] . '</p>';
+            }
+
+            $credit_card_fields = array(
+                'creditcardcvc'        => $field[ 'cc_cvc_label' ],
+                'creditcardfullname'   => $field[ 'cc_name_label' ],
+                'creditcardexpiration' => $field[ 'cc_exp_month_label' ] . ' ' . $field[ 'cc_exp_year_label' ],
+                'creditcardzip'        => __( 'Credit Card Zip', 'ninja-forms' ),
+            );
+
+
+            foreach( $credit_card_fields as $new_type => $new_label ){
+                $field[ 'new_fields' ][] = array_merge( $field, array(
+                    'id' => '',
+                    'type' => $new_type,
+                    'label' => $new_label,
+                    'help_text' => '',
+                    'desc_text' => ''
+                ));
+            }
+        }
+
+        /*
+         * Convert inside label position over to placeholder
+         */
+        if ( isset ( $field[ 'label_pos' ] ) && 'inside' == $field[ 'label_pos' ] ) {
+            if ( ! isset ( $field[ 'placeholder' ] ) || empty ( $field[ 'placeholder' ] ) ) {
+                $field[ 'placeholder' ] = $field[ 'label' ];
+            }
+            $field[ 'label_pos' ] = 'hidden';
+        }
+
+        if( isset( $field[ 'desc_text' ] ) ){
+            $field[ 'desc_text' ] = nl2br( $field[ 'desc_text' ] );
+        }
+        if( isset( $field[ 'help_text' ] ) ){
+            $field[ 'help_text' ] = nl2br( $field[ 'help_text' ] );
+        }
+
+
+        return apply_filters( 'ninja_forms_upgrade_field', $field );
     }
 }
