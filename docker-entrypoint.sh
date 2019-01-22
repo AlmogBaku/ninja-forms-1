c1d43fe781af146041a9801944f35a7bcb75089f
Kyle Johnson <kyle@saturdaydrive.io>
2019-01-22 14:27:24 -0500

    First pass at folding in acceptance tests.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Allows WP CLI to run with the right permissions.
+wp-su() {
+    sudo -E -u www-data wp "$@"
+}
+
+# Clean up from previous tests
+rm -rf /wp-core/wp-content/plugins/ninja-forms
+
+
+# Make sure permissions are correct.
+cd /wp-core
+chown www-data:www-data wp-content/plugins
+chmod 755 wp-content/plugins
+
+# Make sure the database is up and running.
+while ! mysqladmin ping -hmysql --silent; do
+
+    echo 'Waiting for the database'
+    sleep 1
+
+done
+
+echo 'The database is ready'
+
+# Make sure WordPress is installed.
+if ! $(wp-su core is-installed); then
+
+    echo "Installing WordPress"
+
+    wp-su core install --url=wordpress --title=tests --admin_user=admin --admin_password=password --admin_email=test@test.com
+
+    # The development version of Gravity Flow requires SCRIPT_DEBUG
+    wp-su core config --dbhost=mysql --dbname=wordpress --dbuser=root --dbpass=wordpress --extra-php="define( 'SCRIPT_DEBUG', true );" --force
+fi
+
+echo "Grabbing the latest development branch of Ninja Forms"
+git clone -b develop --single-branch https://git.saturdaydrive.io/_/ninja-forms/ninja-forms.git wp-content/plugins/ninja-forms
+
+echo 'Activating Ninja Forms'
+wp-su plugin activate ninja-forms
+
+cd /project
+
+exec "/repo/vendor/bin/codecept" "$@"
