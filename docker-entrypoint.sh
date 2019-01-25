2eb86b7adc4056401f2a297e54bf02070fead919
Kevin Stover <kstover@gmail.com>
2019-01-25 16:11:50 -0500

    Trying to symlink /project/repo/ to /wp-content/plugins/ instead of cloning via git.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Allows WP CLI to run with the right permissions.
+wp-su() {
+    sudo -E -u www-data wp "$@"
+}
+
+# Clean up from previous tests
+rm -rf /wp-core/wp-content/plugins/ninja-forms
+
+
+# Make sure permissions are correct.
+cd /wp-core
+chown www-data:www-data wp-content/plugins
+chmod 755 wp-content/plugins
+
+# Make sure the database is up and running.
+while ! mysqladmin ping -hmysql --silent; do
+
+    echo 'Waiting for the database'
+    sleep 1
+
+done
+
+echo 'The database is ready'
+
+# Make sure WordPress is installed.
+if ! $(wp-su core is-installed); then
+
+    echo "Installing WordPress"
+
+    wp-su core install --url=wordpress --title=tests --admin_user=admin --admin_password=password --admin_email=test@test.com
+
+    # The development version of Gravity Flow requires SCRIPT_DEBUG
+    wp-su core config --dbhost=mysql --dbname=wordpress --dbuser=root --dbpass=wordpress --extra-php="define( 'SCRIPT_DEBUG', true );" --force
+fi
+
+echo "Creating Ninja Forms SymLink"
+# git clone -b develop --single-branch https://git.saturdaydrive.io/_/ninja-forms/ninja-forms.git wp-content/plugins/ninja-forms
+ln -s /project/repo /wp-content/plugins/ninja-forms/
+
+echo 'Activating Ninja Forms'
+wp-su plugin activate ninja-forms
+
+cd /project
+
+exec "/repo/vendor/bin/codecept" "$@"
