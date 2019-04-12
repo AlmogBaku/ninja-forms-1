cd51f2d6657b92e30ebfc7545aef2852d2a92956
Kyle Johnson <kyle@saturdaydrive.io>
2019-04-12 14:04:37 -0400

    Encode/Decode form/field IDs with instance IDs.

--- includes/AJAX/Controllers/Submission.php
+++ includes/AJAX/Controllers/Submission.php
@@ -50,6 +50,20 @@ class NF_AJAX_Controllers_Submission extends NF_Abstracts_Controller
 
         $this->_form_id = $this->_form_data['id'];
 
+        /* Render Instance Fix */
+        if(strpos($this->_form_id, '_')){
+            list($this->_form_id) = explode('_', $this->_form_id);
+
+            $updated_fields = array();
+            foreach($this->_form_data['fields'] as $field_id => $field ){
+                list($field_id) = explode('_', $field_id);
+                list($field['id']) = explode('_', $field['id']);
+                $updated_fields[$field_id] = $field;
+            }
+            $this->_form_data['fields'] = $updated_fields;
+        }
+        /* END Render Instance Fix */
+
         // If we don't have a numeric form ID...
         if ( ! is_numeric( $this->_form_id ) ) {
             // Kick the request out without processing.
