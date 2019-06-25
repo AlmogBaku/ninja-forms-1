3a48dfbef8b540891235b7dbe67db975e8e78337
Kyle Johnson <kyle@saturdaydrive.io>
2019-06-25 11:34:53 -0400

    Update db_version in SQL dump.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,6 @@ rm -rf /wp-core/wp-content/plugins/ninja-forms
 cd /wp-core
 chown www-data:www-data wp-content/plugins
 chmod 755 wp-content/plugins
-chmod 755 wp-includes/version.php
 
 # Make sure the database is up and running.
 while ! mysqladmin ping -hmysql --silent; do
