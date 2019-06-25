18099da5bc2567cd2b912323892bd715c761abe6
Kyle Johnson <kyle@saturdaydrive.io>
2019-06-25 12:31:40 -0400

    Copy repo, recurrsive.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -42,7 +42,7 @@ echo "Creating Ninja Forms SymLink Directory"
 mkdir wp-content/plugins/ninja-forms
 # git clone -b develop --single-branch https://git.saturdaydrive.io/_/ninja-forms/ninja-forms.git wp-content/plugins/ninja-forms
 # ln -s /repo/* wp-content/plugins/ninja-forms/
-cp /repo wp-content/plugins/ninja-forms
+cp -r /repo wp-content/plugins/ninja-forms
 
 echo 'Activating Ninja Forms'
 wp-su plugin activate ninja-forms
