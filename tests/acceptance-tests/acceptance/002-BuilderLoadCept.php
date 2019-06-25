52ba2d082893fe60d7b40b6db151526836b00bc8
Kyle Johnson <kyle@saturdaydrive.io>
2019-06-25 12:01:00 -0400

    Tweak builder load test.

--- tests/acceptance-tests/acceptance/002-BuilderLoadCept.php
+++ tests/acceptance-tests/acceptance/002-BuilderLoadCept.php
@@ -4,6 +4,11 @@ $I = new AcceptanceTester( $scenario );
 // Login to wp-admin
 $I->loginAsAdmin();
 
+$I->wantTo( 'confirm that the dashboard loads properly' );
+$I->amOnPage( '/wp-admin/admin.php?page=ninja-forms' );
+$I->waitForText( 'Add New' );
+
+// $I->amOnPage( '/wp-admin/admin.php?page=ninja-forms&form_id=new' );
 $I->wantTo( 'confirm that the builder loads properly' );
-$I->amOnPage( '/wp-admin/admin.php?page=ninja-forms&form_id=new' );
+$I->click('button.add');
 $I->waitForText( 'Emails & Actions' );
