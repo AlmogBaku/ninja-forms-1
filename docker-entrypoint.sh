63c997379e3fcc7cd84e38dc6e34487b529485f8
Kyle Johnson <kyle@saturdaydrive.io>
2019-06-25 12:35:46 -0400

    Try cloing current branch.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -40,9 +40,9 @@ wp-su core update-db
 
 echo "Creating Ninja Forms SymLink Directory"
 mkdir wp-content/plugins/ninja-forms
-# git clone -b develop --single-branch https://git.saturdaydrive.io/_/ninja-forms/ninja-forms.git wp-content/plugins/ninja-forms
+git clone -b $CI_COMMIT_REF_NAME  --single-branch https://git.saturdaydrive.io/_/ninja-forms/ninja-forms.git wp-content/plugins/ninja-forms
 # ln -s /repo/* wp-content/plugins/ninja-forms/
-cp -r /repo wp-content/plugins/ninja-forms
+# cp -r /repo wp-content/plugins/ninja-forms
 
 echo 'Activating Ninja Forms'
 wp-su plugin activate ninja-forms
