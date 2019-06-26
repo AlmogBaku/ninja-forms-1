5288cdc6cd34252a7e07b7f547756e0232b9d155
Kyle Johnson <kyle@saturdaydrive.io>
2019-06-26 12:38:33 -0400

    Update tests to reflect bypassed modal and updates.

--- tests/acceptance-tests/acceptance/001-DashboardLoadCept.php
+++ tests/acceptance-tests/acceptance/001-DashboardLoadCept.php
@@ -9,16 +9,5 @@ $I->amOnPage( '/wp-admin/plugins.php' );
 $I->see( 'Ninja Forms' );
 $I->see( 'Ninja Forms is a webform builder with unparalleled ease of use and features.' );
 
-$I->wantTo( 'confirm that the dashboard loads properly' );
-$I->amOnPage( '/wp-admin/admin.php?page=ninja-forms' );
-$I->waitForText( 'Help make Ninja Forms better!' );
-$I->click('#optout');
-
-$I->amOnPage( '/wp-admin/admin.php?page=ninja-forms' );
-$I->waitForText( 'Required Updates' );
-$I->click('#nf-required-updates-btn');
-$I->wait('20');
-// $I->waitForText( 'Go To Dashboard' );
-
 $I->amOnPage( '/wp-admin/admin.php?page=ninja-forms' );
 $I->waitForText( 'Add New' );
--- tests/acceptance-tests/acceptance/002-BuilderLoadCept.php
+++ tests/acceptance-tests/acceptance/002-BuilderLoadCept.php
@@ -4,15 +4,6 @@ $I = new AcceptanceTester( $scenario );
 // Login to wp-admin
 $I->loginAsAdmin();
 
-$I->wantTo( 'confirm that the dashboard loads properly' );
-$I->amOnPage( '/wp-admin/plugins.php' );
-$I->see( 'Ninja Forms' );
-$I->see( 'Ninja Forms is a webform builder with unparalleled ease of use and features.' );
-
-$I->wantTo( 'confirm that the dashboard loads properly' );
-$I->amOnPage( '/wp-admin/admin.php?page=ninja-forms' );
-$I->waitForText( 'Add New' );
-
 $I->wantTo( 'confirm that the builder loads properly' );
 $I->amOnPage( '/wp-admin/admin.php?page=ninja-forms&form_id=new' );
 $I->waitForText( 'Emails & Actions' );
