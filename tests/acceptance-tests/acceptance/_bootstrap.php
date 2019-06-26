7a21729dcaffa48fbe977a538ac5c62e1c559517
Kyle Johnson <kyle@saturdaydrive.io>
2019-06-26 12:25:24 -0400

    Relocate db option bootstrapping.

--- tests/acceptance-tests/acceptance/_bootstrap.php
+++ tests/acceptance-tests/acceptance/_bootstrap.php
@@ -1,2 +1,7 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+update_option('ninja_forms_db_version', '1.4');
+update_option('ninja_forms_needs_updates', 0);
+update_option('ninja_forms_allow_tracking', false);
+update_option('ninja_forms_do_not_allow_tracking', true);
+update_option('ninja_forms_optin_reported', 0);
