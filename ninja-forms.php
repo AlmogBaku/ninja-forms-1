45460ed467efa9e95189979bc883253f886a37b9
KR Moorhouse <spkr4thded@gmail.com>
2020-02-03 14:37:57 -0500

    Merged readme-overhaul into sec-hf-stable.

--- ninja-forms.php
+++ ninja-forms.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Ninja Forms
-Plugin URI: http://ninjaforms.com/
+Plugin URI: http://ninjaforms.com/?utm_source=Ninja+Forms+Plugin&utm_medium=readme
 Description: Ninja Forms is a webform builder with unparalleled ease of use and features.
 Version: 3.4.22
-Author: The WP Ninjas
+Author: Saturday Drive
 Author URI: http://ninjaforms.com/?utm_source=Ninja+Forms+Plugin&utm_medium=Plugins+WP+Dashboard
 Text Domain: ninja-forms
 Domain Path: /lang/
