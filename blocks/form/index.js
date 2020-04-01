5830f750e1c00429e6ff0c2e2f2e3bb32a5249bb
Josh Pollock <jpollock412@gmail.com>
2020-04-01 15:13:49 -0400

    Pass props from edit callback to Edit component in form block #4198

--- blocks/form/index.js
+++ blocks/form/index.js
@@ -1,14 +1,23 @@
-import Edit from './Edit';
-import  block from './block';
-import { __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
+import Edit from "./Edit";
+import block from "./block";
+import { __ } from "@wordpress/i18n";
+import { registerBlockType } from "@wordpress/blocks";
 
-registerBlockType('ninja-forms/form',{
-    ...block,
-    title: __('Ninja Form', 'ninja-forms'),
-    edit: ({attributes,setAttributes}) => {
-
-        return <Edit/>
-    },
-    save: () => null
+//Saved forms should have been set in this variable using wp_localize_script
+const nfFormsBlock = window.nfFormsBlock || {};
+const { forms } = nfFormsBlock;
+registerBlockType("ninja-forms/form", {
+	...block,
+	title: __("Ninja Form", "ninja-forms"),
+	edit: ({ attributes, setAttributes }) => {
+		return (
+			<Edit
+				forms={forms}
+				formId={attributes.formId}
+				formTitle={attributes.formTitle}
+				setAttributes={setAttributes}
+			/>
+		);
+	},
+	save: () => null
 });
