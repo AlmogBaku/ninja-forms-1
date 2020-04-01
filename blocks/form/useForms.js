498c9e5e279cb9b5b83b67713cdd1f6bac039516
Josh Pollock <jpollock412@gmail.com>
2020-04-01 14:26:11 -0400

    Impliment useForms hook for form block #4198

--- blocks/form/useForms.js
+++ blocks/form/useForms.js
@@ -1,8 +1,43 @@
+import React from "react";
+/**
+ * Hook to manage a simple collection of forms.
+ *
+ * Forms are an array of {formId: String, formTitle: String} objects.
+ */
 export default function(props) {
-	const [forms] = React.useState(props.forms);
-	function getFormTitle(formId) {}
+	const [forms] = React.useState(() => {
+		return Array.isArray(props.forms)
+			? props.forms
+			: Object.values(props.forms);
+	});
+	/**
+	 * Get the title of a form, by id.
+	 */
+	function getFormTitle(formId) {
+		if (!forms.length) {
+			return undefined;
+		}
+		let form = forms.find(f => f.formId === formId);
+		if (form) {
+			return form.formTitle;
+		}
+		return undefined;
+	}
 
-	function asSelectOptions() {}
+	/**
+	 * Get forms as array of ooptions for select control
+	 */
+	function asSelectOptions() {
+		if (!forms.length) {
+			return [];
+		}
+		return forms.map(form => {
+			return {
+				label: form.formTitle,
+				value: form.formId
+			};
+		});
+	}
 
 	return {
 		getFormTitle,
