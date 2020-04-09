f3b47648fcced801293b7a88b25be0d27d595e42
Kyle Johnson <kyle@saturdaydrive.io>
2020-04-09 18:11:35 -0400

    Update file uploads advanced setting modal content.

--- includes/Config/FormSettingsTypes.php
+++ includes/Config/FormSettingsTypes.php
@@ -23,6 +23,8 @@ return apply_filters( 'ninja_forms_from_settings_types', array(
         'modal-content'                 => '<div class="available-settings-modal">
                                                 <img src="' . Ninja_Forms::$url . 'assets/img/add-ons/file-uploads.png"/>
                                                 <p>In order to use this template, you need File Uploads for Ninja Forms.</p>
+                                                <p>
+                                                Let users upload any file to your website with File Uploads! Restrict by file type and size. Upload to server, media library, or your favorite cloud service.</p>
                                                 <div class="actions">
                                                     <a target="_blank" href="https://ninjaforms.com/extensions/stripe/?utm_source=Ninja+Forms+Plugin&utm_medium=Add+New&utm_campaign=Dashboard+New+Form+Template&utm_content=File+Upload
                                                     " title="File Uploads" class="primary nf-button">Learn More</a>
