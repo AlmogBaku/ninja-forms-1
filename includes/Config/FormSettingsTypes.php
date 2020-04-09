2d40d647940ce5b5da67e50ef0125675b786998c
Kyle Johnson <kyle@saturdaydrive.io>
2020-04-09 18:09:52 -0400

    Add File Uploads advanced setting modal.

--- includes/Config/FormSettingsTypes.php
+++ includes/Config/FormSettingsTypes.php
@@ -17,4 +17,17 @@ return apply_filters( 'ninja_forms_from_settings_types', array(
     	'nicename' 		=> esc_html__( 'Calculations', 'ninja-forms')
     ),
 
+    'uploads' 		=> array(
+    	'id' 			=> 'uploads',
+        'nicename' 		=> esc_html__( 'File Uploads', 'ninja-forms'),
+        'modal-content'                 => '<div class="available-settings-modal">
+                                                <img src="' . Ninja_Forms::$url . 'assets/img/add-ons/file-uploads.png"/>
+                                                <p>In order to use this template, you need File Uploads for Ninja Forms.</p>
+                                                <div class="actions">
+                                                    <a target="_blank" href="https://ninjaforms.com/extensions/stripe/?utm_source=Ninja+Forms+Plugin&utm_medium=Add+New&utm_campaign=Dashboard+New+Form+Template&utm_content=File+Upload
+                                                    " title="File Uploads" class="primary nf-button">Learn More</a>
+                                                </div>
+                                            </div>',
+    ),
+
 ));
